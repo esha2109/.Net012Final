29834950cc81fdd2453463e948e052cfa82cb7b9
Syeda Rusdana <rusdana2109@gmail.com>
2020-07-02 20:32:46 -0400

    finals

---
+++ Final.NET012/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Final.NET012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[100];
+            Adult adult = new Adult();
+            Child child = new Child();
+
+            Console.WriteLine(adult.FirstName);
+            Console.WriteLine(adult.LastName);
+            Console.WriteLine(child.FirstName);
+            Console.WriteLine(child.LastName);
+
+        }
+    }
+    public class Adult
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Adult(){
+            this.FirstName = "Syeda";
+            this.LastName = "Esha";
+
+         }
+     }
+
+    public class Child
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Child()
+        {
+            this.FirstName = "Baby";
+            this.LastName = "Jonus";
+
+        }
+
+    }
+}
